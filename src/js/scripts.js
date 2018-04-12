addea892396f44e33a9b9a3979069bf8ddc2571c
pixelaria <info@pixelaria.ru>
2018-04-12 23:19:19 +0300

    Some product page fixes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -25,6 +25,63 @@ $(function (){
     $(this).closest('.nav').toggleClass('nav--active');
   });
   
+  $('.gallery').lightSlider({
+      gallery:true,
+      item:1,
+      loop:true,
+      thumbItem:5,
+      slideMargin:0,
+      enableDrag: false,
+      currentPagerPosition:'left',
+      onSliderLoad: function(el) {
+          el.lightGallery({
+              selector: '.gallery .lslide'
+          });
+      }   
+  });  
+
+
+
+  $(document).on('click','.spinner__button', function(e){
+    var target = $(this).siblings('.spinner__input--main');
+    var change = 1,min = 1, max=200;
+    
+    if (target.data('change') != undefined) change = parseInt(target.data('change'));
+    if (target.data('min') != undefined) min = parseInt(target.data('min'));
+    if (target.data('max') != undefined) max = parseInt(target.data('max'));
+    
+    var val;
+    
+    if ($(this).hasClass('spinner__button--up')) {
+      val=parseInt(target.val()) + change;
+    } else {
+      val=parseInt(target.val()) - change;
+    }
+    
+    if (val<min) val=min;
+    if (val>max) val=max;
+
+    $(target).val(val).change();
+    return false;
+  });
+
+  $(document).on('click','.select', function(e){
+    $(this).toggleClass('select--opened');
+    return false;
+  });
+
+  $(document).on('click','.select__item', function(e){
+    var text = $(this).html();
+    var select = $(this).closest('.select');
+    var value = $(this).data('value');
+    select.find('.select__input').val(value);
+    select.find('.select__input').trigger('change');
+    select.find('.select__placeholder').html(text);
+    select.removeClass('select--opened');
+    return false;
+  });
+
+
   if ($('.baron').length) {
     baron({
         root: '.baron',
